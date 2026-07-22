400406718e347abe893c8ccc601556beda5d23c8
YavorPtv <yavorpetrakiev@gmail.com>
2026-07-22 23:36:40 +0300

    Add CDK infrastructure skeleton

--- infra/cdk/src/index.ts
+++ infra/cdk/src/index.ts
@@ -1,1 +1,12 @@
-export const cdkAppName = "InfraLens CDK";
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { InfraLensStack } from "./infralens-stack";
+
+const app = new cdk.App();
+
+new InfraLensStack(app, "InfraLensStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
